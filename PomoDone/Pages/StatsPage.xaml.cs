d815812f081d858167b467ed364dcf0fbf90f538
Kurt Andrei S. Decano <kurtandreidecano.07@gmail.com>
2026-06-14 12:31:59 +0800

    Revert "Day 5: stats — demo seeder, LiveCharts2 charts, XAML heatmap, PNG export"

--- PomoDone/Pages/StatsPage.xaml.cs
+++ PomoDone/Pages/StatsPage.xaml.cs
@@ -4,17 +4,9 @@ namespace PomoDone.Pages;
 
 public partial class StatsPage : ContentPage
 {
-    private readonly StatsViewModel _viewModel;
-
     public StatsPage(StatsViewModel viewModel)
     {
         InitializeComponent();
-        BindingContext = _viewModel = viewModel;
-    }
-
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-        await _viewModel.LoadAsync();
+        BindingContext = viewModel;
     }
 }
